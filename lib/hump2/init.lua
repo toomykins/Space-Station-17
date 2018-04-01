52ab71f5a464af8fa992bcac5337ee8852903ebb
= <=>
2018-04-01 18:01:28 +0200

    weW

---
+++ lib/hump2/init.lua
@@ -0,0 +1,9 @@
+local BASE = (...) .. '.'
+
+camera = require(BASE .."camera")
+gamestate = require (BASE .. "gamestate")
+signal = require(BASE .. "signal")
+timer = require(BASE .."timer")
+vector = require(BASE .."vector")
+vectorlight = require(BASE.."vector-light")
+class = require(BASE.."class")
